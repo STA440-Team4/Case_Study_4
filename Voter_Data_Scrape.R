fce8c5012d468cafaa29a17c3b9b5f971007d9b8
Catherine Tsang <cjt28@saxon.stat.duke.edu>
2018-10-15 23:02:24 -0400

    failed to scrape :(

--- Voter_Data_Scrape.R
+++ Voter_Data_Scrape.R
@@ -1,1 +1,15 @@
 # Creating a Tidy Data Frame of Voter Data
+
+if (!require("dplyr")) install.packages("dplyr")
+library(dplyr)
+if (!require("rvest")) install.packages("rvest")
+library(rvest)
+
+# Help for scraping and downloading zip files comes from
+# https://stackoverflow.com/questions/46838858/r-web-scrapping-and-downloading-multiple-zip-files-and-save-the-files-without
+
+url = "https://dl.ncsbe.gov/?prefix=data/"
+page = read_html(url)
+find_nodes = html_nodes(page, "a")
+
+zip <- grep("a", html_nodes(page, "a.zip"), value = TRUE)
